b7db9be94d142552ffada96e63806d7d78e433ff
Shaoe_en Hung <lawrence900613@gmail.com>
2026-04-04 11:51:46 -0700

    Add: maximum-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-subarray/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int current = nums[0];
+        int max = current;
+        for (int j = 1; j < nums.length; j++) {
+            // Smaller Case
+            if((nums[j] + current) < nums[j]) {
+                current = nums[j];
+            } else {
+                current = nums[j] + current;
+            }
+             max = Math.max(current, max);
+        }
+        return max;
+    }
+}
